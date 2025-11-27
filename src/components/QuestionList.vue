9424b95bce7319402e127d06c98b18a507a0bd6f
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-27 05:40:55 +0100

    Question list component added

---
+++ src/components/QuestionList.vue
@@ -0,0 +1,24 @@
+<script setup lang="js">
+import { defineProps, ref, watch } from 'vue';
+
+const props = defineProps({
+    questions: {
+        type: Array,
+        required: true
+    },
+
+    startIndex: {
+        type: Number,
+        default: 1
+    }
+});
+
+const questionList = ref(props.questions);
+
+</script>
+
+<template>
+    <li v-for="(question, index) in questionList" :key="index" class="question-item">
+        {{ startIndex + index }}. {{ question }}
+    </li>
+</template>
