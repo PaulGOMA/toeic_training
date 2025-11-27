07bf473f5e40ae3d413eccdb820d3e282d695876
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-27 03:13:01 +0100

    TitleBar component added

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="js">
+import Menubar from 'primevue/menubar';
+
+</script>
+
+<template>
+    <Menubar>
+        <template #start>
+            <h1>TOEIC Training</h1>
+        </template>
+    </Menubar>
+</template>
