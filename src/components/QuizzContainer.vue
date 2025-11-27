1075c4141e43895b95424d26c21b37594b0a2146
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-27 07:50:06 +0100

    Quizz container component added

---
+++ src/components/QuizzContainer.vue
@@ -0,0 +1,28 @@
+<script setup lang="js">
+import { computed, defineProps } from 'vue';
+import Card from 'primevue/card';
+
+const props = defineProps({
+    isQuestionList: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const cardTitle = computed(() => {
+    return props.isQuestionList ? 'Record what these questions imply ' : 
+        'Select the best implication related to the previous questions.';
+});
+
+</script>
+
+<template>
+    <Card class="quizz-container">
+        <template #title>
+            <h3>{{ cardTitle }}</h3>
+        </template>
+        <template #content>
+            <slot></slot>
+        </template>
+    </Card>
+</template>
