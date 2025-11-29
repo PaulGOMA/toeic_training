c4bbd6afe56a9226c03a608eea2e7b07ede3c993
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-29 09:38:45 +0100

    Footer component added

---
+++ src/components/FooterContainer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer-container">
+        <p>Made with love by 
+            <a class="profil-link" href="https://github.com/PaulGOMA" target="_blank">Paul GOMA</a>
+        </p>
+    </footer>
+</template>
