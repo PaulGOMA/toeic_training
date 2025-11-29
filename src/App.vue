b56124683e4df17f1583bda659916f3817853e8a
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-29 09:30:30 +0100

    App layout updated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script setup>
+import TitleBar from './components/TitleBar.vue';
+import FooterContainer from './components/FooterContainer.vue';
+import { RouterView } from 'vue-router';
+
+</script>
+
 <template>
-  <div>Salut</div>
+  <div class="app-container">
+    <title-bar/>
+    <main class="main-content"> 
+      <router-view/>
+    </main>
+    <footer-container/>
+  </div>
 </template>
