3a52ea2b170712aad21c6d054476e24645a5ab22
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-11-29 08:43:18 +0100

    Quizz container component updated

--- src/components/QuizzContainer.vue
+++ src/components/QuizzContainer.vue
@@ -1,17 +1,26 @@
 <script setup lang="js">
 import { computed, defineProps } from 'vue';
 import Card from 'primevue/card';
+import Button from 'primevue/button';
 
 const props = defineProps({
     isQuestionList: {
         type: Boolean,
         default: false
+    },
+    disabledButton: {
+        type: Boolean,
+        default: false
+    },
+    buttonFunction: {
+        type: Function,
+        default: null
     }
 });
 
 const cardTitle = computed(() => {
     return props.isQuestionList ? 'Record what these questions imply ' : 
-        'Select the best implication related to the previous questions.';
+    'Select the best implication related to the previous questions.';
 });
 
 </script>
@@ -23,6 +32,18 @@ const cardTitle = computed(() => {
         </template>
         <template #content>
             <slot></slot>
+        </template>
+        <template #footer>
+            <div class="card-footer">
+                <Button 
+                    label="Next" 
+                    icon="pi pi-angle-right" 
+                    icon-pos="right"  
+                    class="card-button"
+                    :disabled="disabledButton"
+                    @click="buttonFunction"
+                />  
+            </div>  
         </template>
     </Card>
 </template>
